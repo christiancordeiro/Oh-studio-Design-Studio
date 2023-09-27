44b3d40fa1d7edcb09a1abaa6bd41be2cedc997a
Christian Cordeiro <christiancordeiro96@gmail.com>
2023-09-27 18:58:46 -0300

    first commit

---
+++ js/header.js
@@ -0,0 +1,18 @@
+export default function initHeader() {
+  const headerList = document.querySelectorAll("ul li")
+
+  function handleClick(event) {
+    event.preventDefault()
+
+    headerList.forEach((item) => {
+      item.classList.remove("selecionado")
+    })
+
+    const clique = event.target
+    clique.classList.add("selecionado")
+  }
+
+  headerList.forEach((item) => {
+    item.addEventListener("click", handleClick)
+  })
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import initHeader from "./header.js"
+import initScrollAnimation from "./scroll.js"
+
+initHeader()
+// initScrollAnimation()
